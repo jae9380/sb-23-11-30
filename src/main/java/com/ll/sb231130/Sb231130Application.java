abf9bc447c95281e6dcee610925caf233589530c
jae9380 <ljy5314@gmail.com>
2023-11-30 18:07:35 +0900

    setting5

--- src/main/java/com/ll/sb231130/Sb231130Application.java
+++ src/main/java/com/ll/sb231130/Sb231130Application.java
@@ -2,7 +2,9 @@ package com.ll.sb231130;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class Sb231130Application {
 
